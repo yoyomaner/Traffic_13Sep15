89a37ece289e52f5e38ca9f5dd347dcf36ef2b0b
unknown <you@example.com>
2015-09-13 08:39:14 +0700

    design ui

---
+++ app/src/main/java/demanda/s/phakapob/traffic/MainActivity.java
@@ -0,0 +1,13 @@
+package demanda.s.phakapob.traffic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
